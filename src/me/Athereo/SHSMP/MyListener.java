ecd5f2270def7f9b89ba16c534d242108b8398f7
Angelo <70970878+redstripez08@users.noreply.github.com>
2021-04-12 10:27:42 +0800

    added onPlayerJoin, modified onKill

--- src/me/Athereo/SHSMP/MyListener.java
+++ src/me/Athereo/SHSMP/MyListener.java
@@ -7,38 +7,49 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scoreboard.Scoreboard;
 
 import me.Athereo.SHSMP.DiscordWebhook.EmbedObject;
 
 public class MyListener implements Listener {
-	private JavaPlugin plugin;
+	private Main plugin;
 	private MyRecipes recipes;
+	private Scoreboard scoreboard;
 
-	public MyListener(JavaPlugin plugin) {
+	public MyListener(Main plugin) {
 		this.plugin = plugin;
 		this.recipes = new MyRecipes(plugin);
+		this.scoreboard = plugin.scoreboard;
+	}
+
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		Player player = event.getPlayer();
+
+		if (player.isDead()) {
+			scoreboard.getTeam("Dead").addEntry(player.getDisplayName());
+			player.sendMessage("You have been assigned to Dead team");
+		} else {
+			scoreboard.getTeam("Alive").addEntry(player.getDisplayName());
+		}
 	}
 
     @EventHandler
     public void onCraft(CraftItemEvent event) throws Exception {
-		System.out.println(plugin.getConfig().getString("Discord Webhook"));
-		// System.out.println(event.getRecipe().hashCode());
-		// System.out.println(recipes.new RevivalBook().getRecipe().hashCode());
-
         if (event.isCancelled() || !(event.getWhoClicked() instanceof Player)) {
 			return;
 		}
 		
-		if (event.getRecipe().getResult().getItemMeta().getDisplayName() == recipes.new RevivalBook().getItem().getItemMeta().getDisplayName()) {
+		// Going to make it check for lore instead soon, because it is possible
+		// someone just changes the name using Anvil and get 
+		if (event.getRecipe().getResult().getItemMeta().getDisplayName() == recipes.new Necronomicon().getItem().getItemMeta().getDisplayName()) {
 			Player player = (Player) event.getWhoClicked();
 			String msg = ChatColor.translateAlternateColorCodes('&', "&3" + player.getDisplayName() + " &rhas crafted a &l&8Necronomicon.&r");
 			
 			Bukkit.broadcastMessage(msg);
 			sendWebhook(event);
 		}
-	
-		// System.out.println("Something got crafted uwu");
 	}
 
 	@EventHandler
@@ -49,17 +60,18 @@ public class MyListener implements Listener {
 
 		Player player = (Player) event.getEntity();
 		// Player killer = player.getKiller();
+		scoreboard.getTeam("Dead").addEntry(player.getDisplayName());
+		player.sendMessage("You have been assigned to Dead team");
 
-		if (player.isDead()) {
-			Bukkit.broadcastMessage("Big oof. " + player.getDisplayName() + " has died");
-		}
+
+		Bukkit.broadcastMessage("Big oof. " + player.getDisplayName() + " has died");
 	}
 
 
     private void sendWebhook(CraftItemEvent event) throws Exception {
 		Player player = (Player) event.getWhoClicked();
 
-        DiscordWebhook webhook = new DiscordWebhook(plugin.getConfig().getString("Discord Webhook"));
+        DiscordWebhook webhook = new DiscordWebhook(plugin.getConfig().getString("DiscordWebhook"));
 		EmbedObject embed = new EmbedObject()
 			.setTitle(player.getName() + " has Crafted a Necronomicon!")
 			.setDescription("A player can now be revived!");
